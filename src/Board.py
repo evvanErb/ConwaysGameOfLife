2bb773b01c1cc1a3f021ac28468f2186e77bf814
evvanErb <evvan@protonmail.com>
2022-04-06 19:33:02 -0400

    numpy conversion

--- src/Board.py
+++ src/Board.py
@@ -1,67 +1,44 @@
 #!/usr/bin/python3
 import random
+import numpy as np
 from CellStatus import CellStatus
 
 def initializeAcorn():
-    board = []
-    currentGen = []
-    #initialize board
-    for row in range(100):
-        board.append([])
-        currentGen.append([])
-        for column in range(100):
-            board[row].append(CellStatus.DEAD)
-            currentGen[row].append(CellStatus.DEAD)
 
-    currentGen[44][74] = board[44][74] = CellStatus.ALIVE
-    currentGen[44][75] = board[44][75] = CellStatus.ALIVE
-    currentGen[42][75] = board[42][75] = CellStatus.ALIVE
-    currentGen[43][77] = board[43][77] = CellStatus.ALIVE
-    currentGen[44][78] = board[44][78] = CellStatus.ALIVE
-    currentGen[44][79] = board[44][79] = CellStatus.ALIVE
-    currentGen[44][80] = board[44][80] = CellStatus.ALIVE
+    board = np.full((100, 100), CellStatus.DEAD)
+
+    board[44, 74] = CellStatus.ALIVE
+    board[44, 75] = CellStatus.ALIVE
+    board[42, 75] = CellStatus.ALIVE
+    board[43, 77] = CellStatus.ALIVE
+    board[44, 78] = CellStatus.ALIVE
+    board[44, 79] = CellStatus.ALIVE
+    board[44, 80] = CellStatus.ALIVE
     
-    return [board, currentGen]
+    return board
 
 def initializeRandom(seedValue):
 
-    board = []
-    currentGen = []
+    board = np.full((100, 100), CellStatus.DEAD)
     random.seed(seedValue)
 
     for row in range(100):
-        board.append([])
-        currentGen.append([])
         for column in range(100):
             if(random.randint(0,9) == 0):
-                board[row].append(CellStatus.ALIVE)
-                currentGen[row].append(CellStatus.ALIVE)
-            else:
-                board[row].append(CellStatus.DEAD)
-                currentGen[row].append(CellStatus.DEAD)
+                board[row, column] = CellStatus.ALIVE
 
-    return [board, currentGen]
+    return board
 
 def initializeBoard():
 
     seedValue = input("Enter a seed value:\n>>> ")
-    boardAndCurrentGen = []
     
     if(seedValue == "acorn"):
         #initialize board
-        boardAndCurrentGen = initializeAcorn()
+        return initializeAcorn()
         
     else:
-        boardAndCurrentGen = initializeRandom(seedValue)
-
-    return [boardAndCurrentGen[0], boardAndCurrentGen[1]]
-
-#update curent gen board		
-def updateCurrentGen(board, currentGen):
-    for row in range(100):
-        for column in range(100):
-            currentGen[row][column] = board[row][column]
-    return currentGen
+        return initializeRandom(seedValue)
 
 def sum(row, column, currentGen):
     cellSum = 0
@@ -69,7 +46,7 @@ def sum(row, column, currentGen):
         for columnModifier in range(-1,2):
 
             if (not (row + rowModifier < 0 or row + rowModifier > 99 or column + columnModifier < 0 or column + columnModifier > 99)):
-                    if ((currentGen[row+rowModifier][column+columnModifier] == CellStatus.ALIVE) and (rowModifier != 0 or columnModifier != 0)):
+                    if ((currentGen[(row+rowModifier), (column+columnModifier)] == CellStatus.ALIVE) and (rowModifier != 0 or columnModifier != 0)):
                         cellSum += 1
 
     return cellSum
@@ -96,13 +73,13 @@ def rules(row, column, current, currentGen):
 def updateBoard(board, currentGen):
     for row in range(len(board)):
         for column in range(len(board[row])):
-            board[row][column] = rules(row, column, currentGen[row][column], currentGen)
+            board[row, column] = rules(row, column, currentGen[row][column], currentGen)
     return board
 
 def printBoard(board, canvas):
     for row in range(len(board)):
         for column in range(len(board[row])):
-            if (board[row][column] == CellStatus.ALIVE):
+            if (board[row, column] == CellStatus.ALIVE):
                 y = row*10
                 x = column*10
                 canvas.draw_polygon([(x,y),(x,y+10),(x+10,y+10),(x+10,y)], 0 ,"Black","Black")
--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import simpleguitk
+import numpy as np
 from Board import *
 
 def draw_handler(canvas):
-    global currentGen
+    global board, currentGen
     printBoard(board, canvas) 
-    updateBoard(board, currentGen)
-    currentGen = updateCurrentGen(board, currentGen)
+    board = updateBoard(board, currentGen)
+    currentGen = np.copy(board)
 
-boardAndCurrentGen = initializeBoard()
-board = boardAndCurrentGen[0]
-currentGen = boardAndCurrentGen[1]
+board = initializeBoard()
+currentGen = np.copy(board)
 
 frame = simpleguitk.create_frame('Conway\'s Game of Life', 1000, 1000)
 frame.set_canvas_background("White")

f070f4edf177783ef3c5efa55921b9b3bb140a46
evvanErb <evvan@protonmail.com>
2022-04-07 10:44:08 -0400

    refactored main

--- src/main.py
+++ src/main.py
@@ -3,14 +3,23 @@ import simpleguitk
 import numpy as np
 from Board import *
 
-def draw_handler(canvas):
+def main(canvas):
+
     global board
     drawBoard(board, canvas) 
     board = updateBoard(board)
 
-board = initializeBoard()
 
-frame = simpleguitk.create_frame('Conway\'s Game of Life', 1000, 1000)
-frame.set_canvas_background("White")
-frame.set_draw_handler(draw_handler)
-frame.start()
+def init():
+
+    global board
+    board = initializeBoard()
+
+    frame = simpleguitk.create_frame('Conway\'s Game of Life', 1000, 1000)
+    frame.set_canvas_background("White")
+    frame.set_draw_handler(main)
+    frame.start()
+  
+  
+if __name__=="__main__":
+    init()
